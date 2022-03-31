965794ed6430fed581053bd81d84d13b1c46828e
Ting <tchangysl@gmail.com>
2022-03-31 20:30:31 +0800

    Add `wave` function and counter function

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,34 @@
 const main = async () => {
+  const signers = await hre.ethers.getSigners()
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
   const waveContract = await waveContractFactory.deploy()
   await waveContract.deployed()
+
+  const owner = signers[0]
+  signers.shift()
+  const peopleInLineToWave = signers
+
   console.log('Contract deployed to:', waveContract.address)
+  console.log('Contract deployed by:', owner.address)
+
+  let waveCount
+  waveCount = await waveContract.getTotalWaves()
+
+  let waveTxn = await waveContract.wave()
+  await waveTxn.wait()
+
+  waveCount = await waveContract.getTotalWaves()
+
+  console.log(
+    `There are ${peopleInLineToWave.length} people in line to wave! Let's get wavin'`
+  )
+
+  for (const person of peopleInLineToWave) {
+    waveTxn = await waveContract.connect(person).wave()
+    await waveTxn.wait()
+
+    waveCount = await waveContract.getTotalWaves()
+  }
 }
 
 const runMain = async () => {
